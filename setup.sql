771c4c50fc9d9c8efbe188cd17b18c03dce9e1cd
aljazravnjak <aljaz.ravnjak@yahoo.com>
2025-05-15 12:07:58 +0200

    Setup.sql

--- setup.sql
+++ setup.sql
@@ -1,9 +1,11 @@
--- CREATE TABLE insurance (
---     age INTEGER,
---     sex TEXT,
---     bmi NUMERIC,
---     children INTEGER,
---     smoker TEXT,
---     region TEXT,
---     charges NUMERIC
--- );
+--Creating the relevant table in pgAdmin with the corresponding column names
+
+CREATE TABLE insurance (
+    age INTEGER,
+    sex TEXT,
+    bmi NUMERIC,
+    children INTEGER,
+    smoker TEXT,
+    region TEXT,
+    charges NUMERIC
+);
